65314f67cb811a057090a9abffcce6f02a366423
Nikolaeva K <6303252@gmail.com>
2026-04-28 16:06:07 +0300

    add unique css for notification

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -46,7 +46,8 @@ public class DeliveryTest {
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(secondMeetingDate);
         $("button.button").click();
-        $(".notification__content")
+
+        $("[data-test-id='replan-notification'] .notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .should(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
 
